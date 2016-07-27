d4c98e4bd5812b53db588b390391ac8630241953
jirikrejci <jiri.krejci42@gmail.com>
2016-07-27 17:18:01 +0200

    Update1 - 1/2 tutotialu 3a

--- app/src/main/java/com/example/jirka/recycleview/model/ListItem.java
+++ app/src/main/java/com/example/jirka/recycleview/model/ListItem.java
@@ -1,4 +1,5 @@
 package com.example.jirka.recycleview.model;
+
 /**
  * Java Representation of our Data. To be displayed in RecyclerView
  * Created by Jirka on 26.7.2016.
@@ -6,12 +7,43 @@ package com.example.jirka.recycleview.model;
 
 public class ListItem {
     private String title;
+    private String subtitle;
     private int imageResID;
+    private boolean favourite = false;
 
-    public String getTitle() {return title;}
-    public void setTitle(String title) {this.title = title;}
-    public int getImageResID() {return imageResID;}
-    public void setImageResID(int imageResID) {this.imageResID = imageResID;}
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        this.favourite = favourite;
+    }
+
+    // getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImageResID() {
+        return imageResID;
+    }
+
+    public void setImageResID(int imageResID) {
+        this.imageResID = imageResID;
+    }
 
 }
 
